2fb5c7a3add274cb173f6c73976ba9eda05826bb
kenwilcox <wilcoxk@gmail.com>
2014-10-13 17:57:50 -0600

    Added Border and Balls

--- BallChallenge/KXMyScene.m
+++ BallChallenge/KXMyScene.m
@@ -13,6 +13,12 @@
 - (id)initWithSize:(CGSize)size {
   if (self = [super initWithSize:size]) {
     /* Setup your scene here */
+    self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+    
+    // Add Edge/Border
+    SKNode *edge = [SKNode node];
+    edge.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    [self addChild:edge];
     
   }
   return self;
@@ -24,15 +30,30 @@
   for (UITouch *touch in touches) {
     CGPoint location = [touch locationInNode:self];
     
-    SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-    
-    sprite.position = location;
-    
-    SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-    
-    [sprite runAction:[SKAction repeatActionForever:action]];
-    
-    [self addChild:sprite];
+    int random = arc4random_uniform(3);
+    SKSpriteNode *ball;
+    switch (random) {
+      case 0:
+        ball = [SKSpriteNode spriteNodeWithImageNamed:@"8Ball"];
+        ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.size.width * 0.5];
+        ball.physicsBody.mass = 0.8;
+        ball.physicsBody.restitution = 0.2;
+        break;
+      case 1:
+        ball = [SKSpriteNode spriteNodeWithImageNamed:@"BeachBall"];
+        ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.size.width * 0.5];
+        ball.physicsBody.mass = 0.1;
+        ball.physicsBody.restitution = 0.8;
+        break;
+      default:
+        ball = [SKSpriteNode spriteNodeWithImageNamed:@"SoccerBall"];
+        ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.size.width * 0.5];
+        ball.physicsBody.mass = 0.4;
+        ball.physicsBody.restitution = 0.5;
+        break;
+    }
+    ball.position = location;
+    [self addChild:ball];
   }
 }
 
