4318a785a67079c9b9ea082f6f9d31c8f077c174
kenwilcox <wilcoxk@gmail.com>
2014-10-13 17:32:45 -0600

    Initial Setup

--- BallChallenge/KXViewController.m
+++ BallChallenge/KXViewController.m
@@ -33,15 +33,6 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
